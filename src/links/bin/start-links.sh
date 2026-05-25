cbb89bd652eb7549f083d7743107eab2b4b9eb06
Brendan Murty <brendan@murty.au>
2026-05-25 13:50:54 +1000

    Links - Use suggested Docker implementation from https://github.com/karakeep-app/karakeep/tree/main/docker

--- src/links/bin/start-links.sh
+++ src/links/bin/start-links.sh
@@ -12,16 +12,16 @@ LINKS_DIR="$REPO_DIR/src/links"
 mkdir -p "$REPO_DIR/storage/links/"{app,search}
 
 cd "$LINKS_DIR"
-
 cp -n .links.sample.env .links.env
 source .links.env
 
-PORT="$KARAKEEP_PORT" \
-  DISABLE_SIGNUPS="$KARAKEEP_DISABLE_SIGNUPS" \
+PORT="$PORT" \
   OPENAI_API_KEY="$OPENAI_API_KEY" \
+  STORAGE_DIR_APP="$STORAGE_DIR_APP" \
+  STORAGE_DIR_SEARCH="$STORAGE_DIR_SEARCH" \
   MEILI_MASTER_KEY="$MEILI_MASTER_KEY" \
   docker compose \
-  --file "$LINKS_DIR/docker-compose.local.yml" \
+  --file "$LINKS_DIR/docker-compose.yml" \
   up --pull always --build -d
 
-echo "Karakeep started at http://localhost:3333/"
+echo "Karakeep started at http://localhost:3000/"
