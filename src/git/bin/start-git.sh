2f6af5892e457fc13400a100babdecff01ff6d43
Brendan Murty <b@bcm.id.au>
2026-06-11 11:39:57 +1000

    Git - Improve env file load and start processes

--- src/git/bin/start-git.sh
+++ src/git/bin/start-git.sh
@@ -14,19 +14,34 @@ cd "$GIT"
 mkdir -p "$REPO/storage/git/"{app,assets,repos,database}
 mkdir -p "$GIT/custom/conf"
 
-if [ ! -f "$GIT/.git.env" ]; then
-  info "Initialise file: .git.env"
-  cp "$GIT/.git.local.env" "$GIT/.git.env"
+if [ -f "$GIT/.git.env" ]; then
+  echo "Loading variables from '.git.env'"
+
+  source "$GIT/.git.env"
+
+  info "Run: Docker Compose Up"
+
+  export USER_UID="$(id -u)" && \
+    export USER_GID="$(id -g)" && \
+    docker compose \
+      --file "$GIT/docker-compose.local.yml" \
+      --env-file "$GIT/.git.env" \
+      up \
+      --pull always \
+      --build \
+      -d && \
+    success "Git started at http://localhost:$PORT_WEB/"
+else
+  echo "File not found at '.git.env'"
+  echo "Run: Docker Compose Up"
+
+  export USER_UID="$(id -u)" && \
+    export USER_GID="$(id -g)" && \
+    docker compose \
+      --file "$GIT/docker-compose.local.yml" \
+      up \
+      --pull always \
+      --build \
+      -d && \
+    success "Git started at http://localhost:$PORT_WEB/"
 fi
-
-info "Load file: .git.env"
-
-source "$GIT/.git.env"
-
-info "Run: Docker Compose Up"
-
-docker compose \
-  --file "$GIT/docker-compose.local.yml" \
-  --env-file "$GIT/.git.env" \
-  up --pull always --build -d &&
-  success "Git started at http://localhost:$PORT_WEB/"
